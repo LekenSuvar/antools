598d6177abe9f1f8e450a2a0769da40f5d529d8c
LekenSuvar <87902339+LekenSuvar@users.noreply.github.com>
2022-04-21 21:44:05 +0400

    Antools all site

--- js/scripts.js
+++ js/scripts.js
@@ -62,3 +62,19 @@ $(".new__favorite").click(function(){
 $(".new__add").click(function(){
     $(this).toggleClass('choosed');
 });
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
